19c5bbcfc29f8c43d3a2889801314b3b8f23f707
itachi-P <itachip38@gmail.com>
2023-09-02 23:13:47 +0900

    fix: change from button onClick to form onSubmit

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,9 +5,10 @@ type FormPropsType = {
 
 const Form = (props: FormPropsType) => {
 	return (
-		<form>
+		//ブラウザによっては<button onClick>では正しく動作しないので、<form onSubmit>に変更
+		<form onSubmit={props.getWeather}>
 			<input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
-			<button type="submit" onClick={props.getWeather}>GetWeather</button>
+			<button type="submit">Get Weather</button> {/* onClickを削除 */}
 		</form>
 	);
 };
