14da9f9eed7ada853dc327f7f92609117e135215
itachi-P <itachip38@gmail.com>
2023-09-02 22:33:01 +0900

    feat: get weather data from API and test print

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
 import Title from './components/Title';
-import Form from './Form';
+import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
 
+type ResultsStateType = {
+	country: string;
+	cityName: string;
+	temperature: string;
+	conditionText: string;
+	icon: string;
+}
+
 function App() {
-  return (
-    <div className="test">
-		<Title />
-		<Form />
-    </div>
-  );
+	const [city, setCity] = useState<string>("");
+	const [results, setResults] = useState<ResultsStateType>({
+		country: "",
+		cityName: "",
+		temperature: "",
+		conditionText: "",
+		icon: "",
+	});
+	const GetWeather = (e: any) => {
+		// ボタンを押した時にページがリロードされるのを防ぐ
+		e.preventDefault();
+		fetch("https://api.weatherapi.com/v1/current.json?key=e9585ab2b07548dc8cd120216230209&q=London&aqi=no")
+			.then(res => res.json())
+			.then(data => {
+				setResults({
+					country: data.location.country,
+					cityName: data.location.name,
+					temperature: data.current.temp_c,
+					conditionText: data.current.condition.text,
+					icon: data.current.condition.icon,
+				});
+			});
+	};
+
+
+	return (
+		<div className="test">
+			<Title />
+			<Form setCity={setCity} getWeather={GetWeather} />
+			<Results results={results}/>
+		</div>
+	);
 }
 
 export default App;
