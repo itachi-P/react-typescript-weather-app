68b4383a7c824422a0ababfb16c557ab9fd1e6a0
itachi-P <itachip38@gmail.com>
2023-09-03 19:22:00 +0900

    style:change title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,23 +4,7 @@
 // 	);
 // };
 
-/*
-Tips: カッコの種類と英語名
-「小・中・大」は一部でしか通じない呼び方。
-英名か、「丸括弧・波括弧・角括弧」と呼んだ方がまだ誤解がない。
-() = parentheses （丸括弧。「小かっこ」と呼ばれることもある）
-{} = (curly) braces　（波括弧。「中かっこ」と呼ばれることもある）
-[] = (square) brackets　（角括弧。「大かっこ」と呼ばれることもある）
-*/
-
-// returnと中括弧{}を省略して以下のように書くこともできる
-// const Title = () => (
-// 	<h1>React World
-// 		<span>Weather</span>
-// 	</h1>
-// )
-
-//returnの中の要素が1個だけの場合、()も全て省略して以下のように書くこともできる
-const Title = () => <h1>React World <span>Weather</span></h1>;
+// returnと中括弧{}の省略や、更に()も全て省略し以下のように書くこともできる
+const Title = () => <h1>React World Weather <span className="title">with TypeScript</span></h1>;
 
 export default Title;
