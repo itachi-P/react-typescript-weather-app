6c92619b15c56da529610f23daf6c52db3e303d8
itachi-P <itachip38@gmail.com>
2023-09-13 11:46:31 +0900

    style:Line break before subtitle

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,13 @@
-// const Title = () => {
-// 	return (
-// 		<h1>React World Weather</h1>
-// 	);
-// };
+const Title = () => {
+	return (
+		<h1 className="title">
+			React World Weather<br />
+			<span className="title">with TypeScript</span>
+		</h1>
+	);
+};
 
-// returnと中括弧{}の省略や、更に()も全て省略し以下のように書くこともできる
-const Title = () => <h1>React World Weather <span className="title">with TypeScript</span></h1>;
+// returnと中括弧{}、更に()も全て省略し以下のように書くこともできる
+// const Title = () => <h1>React World Weather<br /><span className="title">with TypeScript</span></h1>;
 
 export default Title;
